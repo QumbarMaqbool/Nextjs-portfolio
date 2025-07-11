9ad96db83fc47b9f4306d29c0db8077fe5a18019
Qumbar Maqbool <152381520+qumbarbluemeg@users.noreply.github.com>
2025-07-12 02:07:13 +0500

    fix: changes text

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -70,7 +70,7 @@ function ContactForm() {
 
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
         <p className="text-sm text-[#d3d8e8]">
-          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+          Have questions? I am always open to talk about work opportunities or collaborations.
         </p>
 
         <div className="mt-6 flex flex-col gap-4">

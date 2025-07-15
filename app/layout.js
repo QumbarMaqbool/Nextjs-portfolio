40353248dd7d5f680643ed130693b7843bed13d9
Qumbar Maqbool <152381520+qumbarbluemeg@users.noreply.github.com>
2025-07-15 16:50:37 +0500

    added nav

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          {/* <Navbar /> */}
+          <Navbar />
           {children}
           <ScrollToTop />
         </main>

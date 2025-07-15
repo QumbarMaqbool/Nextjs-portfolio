31dd53741573ae249cd9b3ca18d1fc203ee409d6
Qumbar Maqbool <152381520+qumbarbluemeg@users.noreply.github.com>
2025-07-15 17:32:55 +0500

    edited experience

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import experience from "../../../assets/lottie/code.json";
 // import AnimationLottie from "../../helper/animation-lottie";
-// import GlowCard from "../../helper/glow-card";
+import GlowCard from "../../helper/glow-card";
 
 function Experience() {
   return (
@@ -40,7 +40,7 @@ function Experience() {
           </div>
 
           <div>
-            {/* <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-6">
               {
                 experiences.map(experience => (
                   <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
@@ -74,7 +74,7 @@ function Experience() {
                   </GlowCard>
                 ))
               }
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
